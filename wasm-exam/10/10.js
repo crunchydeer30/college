cf26198dcf8ce86234f52f1277a08c0497577a9d
crunchydeer30 <crunchydeer30@gmail.com>
2024-11-20 08:51:13 +0300

    wasm-exam

--- wasm-exam/10/10.js
+++ wasm-exam/10/10.js
@@ -2,10 +2,13 @@ import fs from "fs/promises";
 
 try {
   const wasmFile = await fs.readFile("./10.wasm");
-  const { instance } = await WebAssembly.instantiate(wasmFile);
-  const { to_binary, memory } = instance.exports;
-
-  console.log(instance.exports.log2(192));
+  const { instance } = await WebAssembly.instantiate(wasmFile, {
+    console,
+  });
+  const { decimalToBinary, memory } = instance.exports;
+  const ptr = decimalToBinary(255);
+  const binary = new Uint8Array(memory.buffer).slice(ptr).join("");
+  console.log(binary);
 } catch (error) {
   console.error(error);
 }
